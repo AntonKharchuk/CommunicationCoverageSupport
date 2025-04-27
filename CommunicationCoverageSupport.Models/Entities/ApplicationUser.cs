6fafa268e86f3ae440220966b060ee03f46df316
Anton Kharchuk <Kharchuk.anton@gmail.com>
2025-04-27 18:56:51 +0300

    Setup Entity Framework Core with MariaDB, completed first database migration

---
+++ CommunicationCoverageSupport.Models/Entities/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿namespace CommunicationCoverageSupport.Models.Entities
+{
+    public class ApplicationUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
---
+++ CommunicationCoverageSupport.Models/Entities/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace CommunicationCoverageSupport.Models.Entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public ICollection<ApplicationUser> Users { get; set; }
+    }
+}
