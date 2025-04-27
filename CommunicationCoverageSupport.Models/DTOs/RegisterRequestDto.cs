8c04571e8300830083654d2e64a8d96e38244d5c
Anton Kharchuk <Kharchuk.anton@gmail.com>
2025-04-27 18:59:04 +0300

    create DTOs

---
+++ CommunicationCoverageSupport.Models/DTOs/RegisterRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CommunicationCoverageSupport.Models.DTOs
+{
+    public class RegisterRequestDto
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
