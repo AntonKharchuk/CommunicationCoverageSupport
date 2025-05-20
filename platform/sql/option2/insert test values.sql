f5e9e547ea91a890d7a0e11379c13efaf23bc69c
Volodymyr Piddubtsev <purdzan@gmail.com>
2025-05-20 12:31:42 +0300

    option 2 final variant

--- platform/sql/option2/insert test values.sql
+++ platform/sql/option2/insert test values.sql
@@ -22,13 +22,12 @@ INSERT INTO owners (id, name) VALUES
 (0, 'NOBODY');
 
 -- Insert test values into the transportKey table
-INSERT INTO owners (id, kInd) VALUES
+INSERT INTO transportKey (id, kInd) VALUES
 (8, 'CONCRETE');
-
 
 SELECT GROUP_CONCAT(column_name) FROM information_schema.columns WHERE table_name = 'simCards';
 SELECT GROUP_CONCAT(column_name) FROM information_schema.columns WHERE table_name = 'simCardsDrain';
 
 #TMP
 (iccid, imsi, msisdn, kIndId, ki1, pin1, pin2, puk1, puk2, adm1, artworkId, accId, installed, cardOwnerId)
-update simCards set installed = 1 where imsi REGEXP '11[0-9]$';
+update simCards set installed = 1 where imsi REGEXP '1[0,1,2,3,4][0-9]$';
